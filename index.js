25efe0d888acf45315180f320abc5b54a87bf276
Ayush Vishwwakarma <ayushvish6555@gmail.com>
2023-12-06 18:52:26 +0530

    addded access control alllow origin

--- index.js
+++ index.js
@@ -5,9 +5,12 @@ const dotenv= require("dotenv")
 dotenv.config();
 
 const app = express() ;
+
+
 app.get("/" , (req, res )=> {
+    res.header("Access-Control-Allow-Origin", "*");
     res.send(json);
-    
+
 })
 
 app.listen(process.env.PORT, ()=> {
